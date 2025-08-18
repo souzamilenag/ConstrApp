89a023a5f563a4423652a6701a5dc61906d771d3
souzamilenag <souza.milenag@uol.com.br>
2025-08-17 22:35:58 -0300

    fix(backend): App.js

--- backend/app.js
+++ backend/app.js
@@ -54,9 +54,10 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'dist')));
+ app.use(express.static(path.join(__dirname, '..', 'dist')));
+
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
     });
 }
 
