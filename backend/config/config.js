2d6c3a9ea644f691f311c8e908e4d200907703d8
souzamilenag <souza.milenag@uol.com.br>
2025-08-17 22:06:00 -0300

    fix(backend): Update production config to use PROD_DB variables and SSL

--- backend/config/config.js
+++ backend/config/config.js
@@ -12,11 +12,17 @@ module.exports = {
   },
 
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    username: process.env.PROD_DB_USER,     
+    password: process.env.PROD_DB_PASSWORD, 
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOST,
+    port: process.env.PROD_DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
