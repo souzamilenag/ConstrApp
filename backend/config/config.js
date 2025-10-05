05f38dc069162cb9baf103257fe21a203f933c5d
souzamilenag <souza.milenag@uol.com.br>
2025-10-05 16:32:25 -0300

    fix SSL

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,25 @@
 // backend/config/config.js
 require('dotenv').config();
 
+const sslConfig = process.env.NODE_ENV === 'production' && process.env.RENDER
+  ? {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    }
+  : {};
+
+
 module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: parseInt(process.env.DB_PORT, 10) || 5432,
     dialect: 'postgres'
   },
 
@@ -16,13 +28,8 @@ module.exports = {
     password: process.env.PROD_DB_PASSWORD,
     database: process.env.PROD_DB_NAME,
     host: process.env.PROD_DB_HOST,
-    port: process.env.PROD_DB_PORT,
+    port: parseInt(process.env.PROD_DB_PORT, 10) || 5432,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false
-      }
-    }
+    ...sslConfig
   }
 };
